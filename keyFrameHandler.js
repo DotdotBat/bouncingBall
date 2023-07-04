9c304184dd244ba9644cfe29ef6f8b52fe3ab393
inPants <skeycoding@gmail.com>
2023-07-04 10:29:01 +0300

    keyFrameArray can be unsorted, at & after for time

--- keyFrameHandler.js
+++ keyFrameHandler.js
@@ -13,12 +13,12 @@
  * 
  * let keyFrames = [
  *    {
- *      time: 0,
+ *      at:0,//exact time
  *      posX: canvasSize.width/2,
  *      posY: canvasSize.height/3
  *    },
  *     {
- *      time: 3,
+ *      after: 3,//after previous keyframe
  *      posX: canvasSize.width,
  *      posY: canvasSize.height,
  *    }
@@ -26,34 +26,50 @@
  * 
  * let interpolatedValue = getValueFromKeyFrames(1.5, 'posX', keyFrames);
  */
-function getValueFromKeyFrames(time, keyName, keyFramesArray ){
-    //we assume that the keyFrames are in chronological order.
-    let previousFrame = null;
-    let nextFrame = null;
-    for (const frame of keyFramesArray) {
-      if (frame[keyName]===undefined) {
-        continue;//skip the frames without this value
-      }
-      if(time<frame.time){
-        nextFrame = frame;
-        break;
-      }
-      previousFrame = frame;
-    }
-    if(previousFrame===null && nextFrame === null){
-      console.error('keyName was not found in keyFrames', keyName, keyFramesArray);
-    }
-    if(previousFrame===null){
-      //so only next frame was found
-      return nextFrame[keyName];
-    }
-    if(nextFrame===null){
-      //so only previous frame was found
-      return previousFrame[keyName];
-    }
-    //found previous and next frame
-    const t = (time - previousFrame.time)/(nextFrame.time - previousFrame.time)
-    const value = lerp(previousFrame[keyName], nextFrame[keyName], t);
-    return value;
+function getValueFromKeyFrames(time, keyName, keyFramesArray) {
+  if(keyFramesArray._wasInitialized===undefined){
+    initializeKeyFramesArray(keyFramesArray);
+    keyFramesArray._wasInitialized = true;
   }
-  
+  let previousFrame = null;
+  let nextFrame = null;
+  for (const frame of keyFramesArray) {
+    if (frame[keyName] === undefined) {
+      continue;//skip the frames without this value
+    }
+    
+    if (time <= frame.time) {
+      nextFrame = frame;
+      break;
+    }
+    previousFrame = frame;
+  }
+  if (previousFrame === null && nextFrame === null) {
+    console.error('keyName was not found in keyFrames', keyName, keyFramesArray);
+  }
+  if (previousFrame === null) {
+    //so only next frame was found
+    return nextFrame[keyName];
+  }
+  if (nextFrame === null) {
+    //so only previous frame was found
+    return previousFrame[keyName];
+  }
+  //found previous and next frame
+  const t = (time - previousFrame.time) / (nextFrame.time - previousFrame.time)
+  const value = lerp(previousFrame[keyName], nextFrame[keyName], t);
+  return value;
+}
+
+
+function initializeKeyFramesArray(arr){
+  //calculate the time for each frame and assign it as a "time property"
+  let previousFrameTime = 0;
+  for(frame of arr){
+    frame.time = frame.at !== undefined ? frame.at : previousFrameTime + frame.after;
+    previousFrameTime = frame.time;
+    console.assert(frame.time !== undefined, "can't get keyFrameTime for keyFrame:", frame);
+  }
+  //sort the keyframes based on the time
+  arr.sort((a,b)=>{a.time-b.time});
+}

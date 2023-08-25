60c92cdeb50ceab011620e0c588f24a25bf84c31
inPants <skeycoding@gmail.com>
2023-08-25 14:05:22 +0300

    visualize angle

--- animations/weightlifting.js
+++ animations/weightlifting.js
@@ -11,26 +11,37 @@ const weightliftingSetup = lp.createForce().afterPrevious().do(() => {
       text("HARDER", width / 2, height / 4);
       blobDog.draw();
       barbell.draw();
-      blobDog.drawPaws();//they are on the barbell, so they have to be drawn on after it
+      blobDog.drawPaws();//they are on top of the barbell, so they have to be drawn after it
+      angleVisualizer(publicAngle);
     }
   });
   
+
+  function angleVisualizer(angle){
+    translate(blobDog.pos.x, blobDog.pos.y);
+    const r = 200; const d=10;
+    circle(r*cos(angle), -r*sin(angle), d);
+  }
+
   const weightliftingSettings = {
     highPoint: 1,//all length values are in relation to blobDog size
     lowPoint: 0.8,
-    period: 60 / song.bpm,
+    period: 2 * 60 / song.bpm,
     barbellAmplitude: 0.05,
     crouchingOffset: Math.PI,
     barbellSwingOffset: Math.PI / 6,
     barbellEndsFollowupTime: 1,
     blobDogStandsOnPoint: { x: canvasSize.width / 2, y: 2 * canvasSize.height / 3 },
   }
-  
-  const weightLiftingLoop = lp.createForce().after(weightliftingSetup).for(3).do(
+  let publicAngle = 0;
+  const weightLiftingLoop = lp.createForce().after(weightliftingSetup).for(4*song.bpm).do(
     () => {
       const wlLoop = weightLiftingLoop;
       const wl = weightliftingSettings;
-      const angle = wlLoop._start + (2 * Math.PI * lp.seconds / wl.period);
+      const angle =  (2 * Math.PI * (lp.seconds-wlLoop._start) / wl.period);
+      {
+        publicAngle = angle;
+      }
       const progress = (1 - cos(angle)) / 2;
       const crouching = wl.lowPoint + (wl.highPoint - wl.lowPoint) * progress;
       blobDog._squeeze = crouching;
@@ -52,5 +63,10 @@ const weightliftingSetup = lp.createForce().afterPrevious().do(() => {
       const leftHoldX = barbell.getLeftHoldX();
       blobDog.takeHoldLeft(leftHoldX, barbell.holdsY);
       blobDog.takeHoldRight(rightHoldX, barbell.holdsY);
+
+      const isRisingMotion = angle % TAU < Math.PI;
+      if(isRisingMotion){
+        blobDog.visibleEffort = true;
+      } else(blobDog.visibleEffort = false);
     }
   );
